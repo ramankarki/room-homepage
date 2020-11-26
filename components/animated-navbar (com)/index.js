481e169b15d964a5de9e6f7fc1fc399311f3372b
ramankarki <ramankarki40@gmail.com>
2020-11-26 21:44:35 +0545

    navbar will not scroll up when it is active

--- components/animated-navbar (com)/index.js
+++ components/animated-navbar (com)/index.js
@@ -4,17 +4,20 @@ const navFixedContainer = document.querySelector(".fixed-container-sm");
 
 AnimatedBtn.addEventListener("click", (e) => {
   navLinks.classList.toggle("nav-links-width");
+  navLinks.classList.toggle("nav-active");
 });
 
 let lastScrolled = 0;
 
 window.addEventListener("scroll", () => {
-  let scrolled = window.screenTop || document.documentElement.scrollTop;
-  console.log(scrolled, lastScrolled);
-  if (scrolled > lastScrolled) {
-    navFixedContainer.style.top = `-${navFixedContainer.clientHeight}px`;
-  } else {
-    navFixedContainer.style.top = `0`;
+  let activeNav = navLinks.className.split(" ").length;
+  if (activeNav === 1) {
+    let scrolled = window.screenTop || document.documentElement.scrollTop;
+    if (scrolled > lastScrolled) {
+      navFixedContainer.style.top = `-${navFixedContainer.clientHeight}px`;
+    } else {
+      navFixedContainer.style.top = `0`;
+    }
+    lastScrolled = scrolled;
   }
-  lastScrolled = scrolled;
 });
